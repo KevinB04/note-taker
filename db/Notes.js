4039a94fb70dfcfb521250dba17380287e27ed8c
Kevin Buttimer <kbuttimer4@gmail.com>
2021-01-19 17:57:05 -0500

    adding .gitignore

--- db/Notes.js
+++ db/Notes.js
@@ -1,6 +1,6 @@
 const util = require("util");
 const fs = require("fs");
-const { v4: uuidv4 } = require('uuid')
+const { v4: uuidv4 } = require('uuid');
 // uuidv4();
 const readAsync = util.promisify(fs.readFile)
 const writeAsync = util.promisify(fs.writeFile)
@@ -20,8 +20,8 @@ class Notes{
                 notesArr = []
             }
             return notesArr;
-        })
-    }
+        });
+    };
 
     addNote(note){
         const { title, text } = note;
@@ -34,12 +34,12 @@ class Notes{
 
         return this.getNotes().then(notesArray => [...notesArray, newNote]).then(newNotes => this.write(newNotes)).then(() => newNote)
 
-    }
+    };
 
     removeNote(id){
         return this.getNotes().then(notesArray => notesArray.filter((note) => note.id !== id)).then(notes => this.write(notes))
     }
-}
+};
 
 module.exports = new Notes();
 
