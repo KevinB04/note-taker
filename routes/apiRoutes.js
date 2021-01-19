61acf1b588b538b16e481151a4f52c364fdb45cb
Kevin Buttimer <kbuttimer4@gmail.com>
2021-01-18 22:24:55 -0500

    adding get, post, and delete functions

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,22 @@
 const path = require("path");
 const fs = require("fs");
+
+app.get('/notes', function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
+
+app.get(`*`, function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+app.get(`/api/notes`, function (req, res) {
+
+})
+
+app.post(`/api/notes`, function (req, res) {
+
+})
+
+app.delete(`/api/notes/:id`, function (req, res) {
+
+})
